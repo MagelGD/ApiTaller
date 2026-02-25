17747add58866647f931651d0e68fa9cba78eeae
INTERRAPIDISIMO\miguelagutierrezg <miguelagutierrezg@interrapidisimo.com>
2026-02-25 17:56:14 -0500

    Se agregan cambios nuevos.

---
+++ ApiTaller.Infrastructure/Helpers/Jwt/GenerateToken.cs
@@ -0,0 +1,27 @@
+﻿using ApiTaller.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTaller.Infrastructure.Helpers.Jwt
+{
+    public class GenerateToken
+    {
+        public GenerateToken()
+        {
+            
+        }
+        private async Task<string> Token(User user, CancellationToken cancellation = default)
+        {
+            try
+            {
+                return "";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "");
+                throw;
+            }
+        }
+    }
+}
