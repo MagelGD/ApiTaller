4ca9cc762bcda5fb3907bbcda9cd614f45139880
INTERRAPIDISIMO\miguelagutierrezg <miguelagutierrezg@interrapidisimo.com>
2026-04-20 17:41:37 -0500

    se agrega cambios nuevos para la nueva tabla de roleaction

---
+++ ApiTaller.Domain/Interfaces/Repositories/RoleActions/IRoleActionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiTaller.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTaller.Domain.Interfaces.Repositories.RoleActions
+{
+    public interface IRoleActionsRepository
+    {
+        Task<List<string>> GetActionsByRoleIdAsync(int roleId, CancellationToken cancellationToken);
+        Task<RoleAction> SaveRoleAction(RoleAction roleAction, CancellationToken cancellationToken);
+        Task<RoleAction> ActiveOrInactiveRoleAction(RoleAction roleAction, CancellationToken cancellationToken);
+    }
+}
