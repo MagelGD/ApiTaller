0f3df6cefa277a1286d308c27f28ba0d20f51f8f
INTERRAPIDISIMO\miguelagutierrezg <miguelagutierrezg@interrapidisimo.com>
2026-04-20 17:46:30 -0500

    se agrega la clase para codificar el repositorio de roleaction

---
+++ ApiTaller.Infrastructure/Data/Repositories/RoleActions/RoleActionsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiTaller.Infrastructure.Data.Repositories.RoleActions
+{
+    internal class RoleActionsRepository
+    {
+    }
+}
